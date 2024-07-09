29f45261c0e592c5e67a3c5408e59bf92c865323
Scodia619 <billyjoe2701@gmail.com>
2024-07-09 23:47:03 +0100

    Changes to Nav on sm screen sizes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 function NavBar() {
+  const [menuActive, setMenuActive] = useState(false);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  console.log(menuActive)
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
-    <div className='navbar d-flex justify-content-between'>
-        <div className="left d-flex justify-content-around align-items-center">
-            <h1>Cheshire Cleaning Supplies</h1>
-            <h2>Contact Us</h2>
-            <h2>Products</h2>
-        </div>
-        <div className="right d-flex justify-content-end">
-            <h2>Login</h2>
-        </div>
+    <div className="navbar-container">
+      <div className="navbar">
+        {isSmallScreen ? (
+          <>
+            <div className="menu-icon" onClick={toggleMenu}>
+              {menuActive ? <FiX size={24} /> : <FiMenu size={24} />}
+            </div>
+            {menuActive && (
+              <div className="dropdown-menu">
+                <h1>Cheshire Cleaning Supplies</h1>
+                <h2>Contact Us</h2>
+                <h2>Products</h2>
+                <h2>Login</h2>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            <div className="left d-flex justify-content-around align-items-center">
+              <h1>Cheshire Cleaning Supplies</h1>
+              <h2>Contact Us</h2>
+              <h2>Products</h2>
+            </div>
+            <div className="right d-flex justify-content-end">
+              <h2>Login</h2>
+            </div>
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
