77c219e0d0445cc20030c82fa54dd2f5a1c2572e
Scodia619 <billyjoe2701@gmail.com>
2024-07-12 19:54:07 +0100

    Basic Tag Filtering for Products

--- src/Components/ProductsContainer.jsx
+++ src/Components/ProductsContainer.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
 import "../Styles/ProductContainer.css"
+import TagQueryDropDown from './TagQueryDropDown';
 
 function ProductsContainer() {
 
-    const products = [
-        {Id: 1, Name: "Blue Roll", Description: "A description of blue roll", Image_Url: "https://www.indooroutdoors.co.uk/cdn/shop/products/phc218c.jpg?v=1661944610"},
-        {Id: 2, Name: "White Roll", Description: "A description of white rollddddddddddddddddddddddd", Image_Url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRq4StzxY42Q8DL4z8H-_QotWGziier20XdM32xQgnqxBSwlU4TpVdp3kscXijwyBnn3rX_REuzsjFuGRbnjndE9mt-KFLAobQKx-O3Tt8vsMVv5wLe0CEFZRqwDkikgPr_KDT8dc0&usqp=CAc7RRvU0oLrkLqqmBpVdH30AAAAA="},
-        {Id: 3, Name: "Sanitizer", Description: "Cleans surfaces", Image_Url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB9mCr4dANqbOJbPxcXE3Few29sUc18nF3LQ&s"}]
-    
+  const products = [
+    {Id: 1, Name: "Blue Roll", Description: "A description of blue roll", Image_Url: "https://www.indooroutdoors.co.uk/cdn/shop/products/phc218c.jpg?v=1661944610", Tag: "Janitor"},
+    {Id: 2, Name: "White Roll", Description: "A description of white rollddddddddddddddddddddddd", Image_Url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRq4StzxY42Q8DL4z8H-_QotWGziier20XdM32xQgnqxBSwlU4TpVdp3kscXijwyBnn3rX_REuzsjFuGRbnjndE9mt-KFLAobQKx-O3Tt8vsMVv5wLe0CEFZRqwDkikgPr_KDT8dc0&usqp=CAc7RRvU0oLrkLqqmBpVdH30AAAAA=", Tag: "Cleaning"},
+    {Id: 3, Name: "Sanitizer", Description: "Cleans surfaces", Image_Url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB9mCr4dANqbOJbPxcXE3Few29sUc18nF3LQ&s", Tag: "Car"}]    
+
+  const [tag, setTag] = useState("All")
+  const [filteredProducts, setFilteredProducts] = useState(products)
+
+
+  useEffect(() => {
+    if (tag === 'All') {
+      setFilteredProducts(products);
+    } else {
+      setFilteredProducts(products.filter(product => product.Tag === tag));
+    }
+  }, [tag]);
+
+
   return (
-    <div className="product-card-container">
-        {products.map(product => {
-        return <ProductCard  key={product.Id} Product={product}/>
-      })}
+    <div className="product-container">
+      <TagQueryDropDown setTag={setTag} Tag={tag}/>
+      <div className="product-card-container">
+          {filteredProducts.map(product => {
+          return <ProductCard  key={product.Id} Product={product}/>
+        })}
+    </div>
     </div>
   )
 }
---
+++ src/Components/TagQueryDropDown.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+
+function TagQueryDropDown({setTag, Tag}) {
+
+    const handleTag = (e) => {
+        setTag(e.target.value)
+    }
+
+    return (
+        <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Filter By</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={Tag}
+          label="Options"
+          onChange={handleTag}
+        >
+          <MenuItem value="All">All</MenuItem>
+          <MenuItem value="Janitor">Janitor</MenuItem>
+          <MenuItem value="Cleaning">Cleaning</MenuItem>
+          <MenuItem value="Car">Car</MenuItem>
+        </Select>
+      </FormControl>
+      );
+}
+
+export default TagQueryDropDown
